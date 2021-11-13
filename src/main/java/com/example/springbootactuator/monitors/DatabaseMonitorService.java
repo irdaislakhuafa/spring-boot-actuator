921928f158e40e57a831935fb81f459af7b8aecb
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2021-11-13 23:45:49 +0700

    Add SystemMonitorService and OtherMonitorService

--- src/main/java/com/example/springbootactuator/monitors/DatabaseMonitorService.java
+++ src/main/java/com/example/springbootactuator/monitors/DatabaseMonitorService.java
@@ -10,7 +10,7 @@ public class DatabaseMonitorService implements HealthIndicator {
     @Override
     public Health health() {
         // TODO Auto-generated method stub
-        return isDatabaseFine() ? Health.up().withDetail("DatabaseStatus", "Service Status is Running").build() : Health.down().withDetail("DatabaseStatus", "Service Status is not Running").build();
+        return isDatabaseFine() ? Health.up().withDetail("DatabaseStatus", "Running").build() : Health.down().withDetail("DatabaseStatus", "Not Running").build();
     }
 
     private Boolean isDatabaseFine(){
